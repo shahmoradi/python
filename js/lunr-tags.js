94490b29053d1608cd7da080f5464435408d9da0
Amir Shahmoradi <a.shahmoradi@gmail.com>
2026-05-24 23:32:23 -0500

    latest build

--- js/lunr-tags.js
+++ js/lunr-tags.js
@@ -809,7 +809,7 @@ $(document).ready(function() {
 
         <div class="col-lg-12 footer">
             &copy;2026 <a href="https://www.cdslab.org" target="_blank">Computational Data Science Lab</a><br />
-            Site last generated: May 20, 2026 <br />
+            Site last generated: May 24, 2026 <br />
             
         <p>
             <a href="https://www.cdslab.org" target="_blank"><img class="footerlogo" src="/python/images/logo.png" }}" alt="cdslab.org" /></a>
